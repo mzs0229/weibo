ba08686cda665ea6f993c733eb36d635ac2057d9
mzs0229 <mzs0229@hotmail.com>
2019-12-11 14:57:37 +0000

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    weibo
+                    
                 </div>
 
                 <div class="links">
