f4f610d9b5473e8fd14beee2b40a542c560533e2
mzs0229 <mzs0229@hotmail.com>
2019-12-15 16:31:37 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Notifications\Notifiable;
 
 use Illuminate\Support\Str;
 
+use Auth;
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -61,11 +63,7 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
-    public function feed()
-    {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
-    }
+    
 
     public function followers()
     {
@@ -96,5 +94,14 @@ class User extends Authenticatable
     public function isFollowing($user_id)
     {
         return $this->followings->contains($user_id);
+    }
+
+    public function feed()
+    {
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+                              ->with('user')
+                              ->orderBy('created_at', 'desc');
     }
 }
